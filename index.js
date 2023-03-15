f2b814537bf0fba1c191e83303f31c1c5498d1ad
Angelina Yumatova <>
2023-03-15 16:33:20 +0100

    change

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('funguju!');
+let vteriny = prompt('Napis za kolik vteřin má uplynout než minutka začne zvonit')
+
+const PridejClassZvuk =() =>{
+    let budik = document.querySelector('.alarm')
+    budik.classList.add = 'alarm--ring'
+    let audio = document.querySelector(audio)
+    audio.src = 'alarm.mp3' 
+    audio.play()
+}
+const ZastavBudik=()=>{
+    audio.stop()
+}
+setTimeout =(
+    PridejClassZvuk,
+    vteriny)
+
+const btnElm = document.querySelector('.tlacitko1')
+btnElm.addEventListener('click', setTimeout)
+const btnElm2 = document.querySelector('.tlacitko2')
+btnElm2.addEventListener('click', ZastavBudik)
+
