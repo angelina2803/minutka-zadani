ee1ca18bba084fbdf42e052c4b36c28de49ef77b
Angelina Yumatova <>
2023-03-25 11:47:04 +0100

    change

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ let vteriny = prompt('Napis za kolik vteřin má uplynout než minutka začne zv
 
 const PridejClassZvuk =() =>{
     let budik = document.querySelector('.alarm')
-    budik.classList.add = 'alarm--ring'
-    let audio = document.querySelector(audio)
+    budik.classList.add('alarm--ring')
+    let audio = document.querySelector('audio')
     audio.src = 'alarm.mp3' 
     audio.play()
 }
 const ZastavBudik=()=>{
-    audio.stop()
+    const audio = document.querySelector("#audio");
+    audio.pause();
+    audio.currentTime = 0;
 }
-setTimeout =(
-    PridejClassZvuk,
-    vteriny)
+setTimeout(PridejClassZvuk,vteriny)
 
 const btnElm = document.querySelector('.tlacitko1')
 btnElm.addEventListener('click', setTimeout)
 const btnElm2 = document.querySelector('.tlacitko2')
 btnElm2.addEventListener('click', ZastavBudik)
-
