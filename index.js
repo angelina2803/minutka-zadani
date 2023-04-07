323439d65fa56fcb50e5291e5d693bafce014e61
Angelina Yumatova <>
2023-04-07 17:13:28 +0200

    change

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
-let vteriny = prompt('Napis za kolik vteřin má uplynout než minutka začne zvonit')
+// const vteriny = prompt('Napis za kolik vteřin má uplynout než minutka začne zvonit')
 
-const PridejClassZvuk =() =>{
-    let budik = document.querySelector('.alarm')
-    budik.classList.add('alarm--ring')
-    let audio = document.querySelector('audio')
-    audio.src = 'alarm.mp3' 
-    audio.play()
-}
-const ZastavBudik=()=>{
-    const audio = document.querySelector("#audio");
-    audio.pause();
-    audio.currentTime = 0;
-}
-setTimeout(PridejClassZvuk,vteriny)
+// const PridejClassZvuk =() =>{
+//     let budik = document.querySelector('.alarm')
+//     budik.classList.add('alarm--ring')
+//     let audio = document.querySelector('audio')
+//     audio.src = 'alarm.mp3' 
+//     audio.play()
+// }
+// const ZastavBudik=()=>{
+//     const audio = document.querySelector("#audio");
+//     audio.pause();
+//     audio.currentTime = 0;
+// }
+// setTimeout(PridejClassZvuk,vteriny)
 
-const btnElm = document.querySelector('.tlacitko1')
-btnElm.addEventListener('click', setTimeout)
-const btnElm2 = document.querySelector('.tlacitko2')
-btnElm2.addEventListener('click', ZastavBudik)
+// const btnElm = document.querySelector('.tlacitko1')
+// btnElm.addEventListener('click', setTimeout)
+// const btnElm2 = document.querySelector('.tlacitko2')
+// btnElm2.addEventListener('click', ZastavBudik)
+let userDelay;
+
+const PridejClassZvuk = () => {
+  let budik = document.querySelector('.alarm');
+  budik.classList.add('alarm--ring');
+};
+
+const PlaySound = () => {
+  let audio = document.querySelector('audio');
+  audio.src = 'alarm.mp3';
+  audio.play();
+};
+
+const ZastavBudik = () => {
+  const audio = document.querySelector("#audio");
+  audio.pause();
+  audio.currentTime = 0;
+  let budik = document.querySelector('.alarm');
+  budik.classList.remove('alarm--ring');
+};
+
+let timeoutId;
+
+const ZacitBudik = () => {
+  clearTimeout(timeoutId);
+  userDelay = parseInt(prompt('Napis za kolik vteřin má uplynout než minutka začne zvonit'), 10) * 1000;
+  timeoutId = setTimeout(() => {
+    PridejClassZvuk();
+    PlaySound();
+  }, userDelay);
+};
+
+const btnElm = document.querySelector('.tlacitko1');
+btnElm.addEventListener('click', ZacitBudik);
+
+const btnElm2 = document.querySelector('.tlacitko2');
+btnElm2.addEventListener('click', () => {
+  clearTimeout(timeoutId);
+  ZastavBudik();
+});
